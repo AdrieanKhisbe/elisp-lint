f7b3690ec6fc4c2486b60d6054fb7347118f5e53
Adriean Khisbe <adriean.khisbe@live.fr>
2015-06-02 15:59:56 +0200

    add scaffold ert test

---
+++ test/elisp-lint-test.el
@@ -0,0 +1,10 @@
+;;; Test for `elisp-lint'
+
+;;; Commentary:
+;; These are the tests for `elisp-lint'
+
+;;; Code:
+
+(ert-deftest elisp-lint-should-not-pass ()
+  (should-not nil))
+
---
+++ test/test-helper.el
@@ -0,0 +1,38 @@
+;;; test-helper --- Test helper for elisp-lint
+
+;;; Commentary:
+;; test helper inspired from https://github.com/tonini/overseer.el/blob/master/test/test-helper.el
+
+;;; Code:
+
+(require 'f)
+
+(defvar cpt-path
+  (f-parent (f-this-file)))
+
+(defvar elisp-lint-test-path
+  (f-dirname (f-this-file)))
+
+(defvar elisp-lint-root-path
+  (f-parent elisp-lint-test-path))
+
+(defvar elisp-lint-sandbox-path
+  (f-expand "sandbox" elisp-lint-test-path))
+
+(when (f-exists? elisp-lint-sandbox-path)
+  (error "Something is already in %s. Check and destroy it yourself" elisp-lint-sandbox-path))
+
+(defmacro within-sandbox (&rest body)
+  "Evaluate BODY in an empty sandbox directory."
+  `(let ((default-directory elisp-lint-sandbox-path))
+     (when (f-exists? elisp-lint-sandbox-path)
+       (f-delete default-directory :force))
+     (f-mkdir elisp-lint-sandbox-path)
+     ,@body
+     (f-delete default-directory :force)))
+
+(require 'ert)
+(require 'elisp-lint (f-expand "elisp-lint" elisp-lint-root-path))
+
+(provide 'test-helper)
+;;; test-helper.el ends here
